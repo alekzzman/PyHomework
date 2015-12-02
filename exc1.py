4dc3bac0f1b298784fb74ba4a36ffdfbf117930b
Alieksieienko Dmytro <alekzzman@gmail.com>
2015-12-02 23:37:13 +0200

    doctest change

--- exc1.py
+++ exc1.py
@@ -1,3 +1,4 @@
+str = 'X-DSPAM-Confidence: 0.8475'
 """
 Task:
 Take the following Python code that stores a string:‘
@@ -8,10 +9,9 @@ of the string after the colon character and then use
 the float function to convert the extracted string into
 a floating point number.
 
->>> str = ’X-DSPAM-Confidence:  0.8475’
+>>> print(d)
 0.8475
 """
-str = 'X-DSPAM-Confidence: 0.8475'
 b = str.find(':') + 1
 c = str[b:]
 d = float(c)
