c3cc186df929430669d33091721ebf344761b60e
Alieksieienko Dmytro <alekzzman@gmail.com>
2015-12-02 23:04:16 +0200

    Python tasks

---
+++ exc1.py
@@ -0,0 +1,18 @@
+"""
+Task:
+Take the following Python code that stores a string:‘
+
+str = ’X-DSPAM-Confidence:  0.8475’
+Use find and string slicing to extract the portion
+of the string after the colon character and then use
+the float function to convert the extracted string into
+a floating point number.
+
+>>> str = ’X-DSPAM-Confidence:  0.8475’
+0.8475
+"""
+str = 'X-DSPAM-Confidence: 0.8475'
+b = str.find(':') + 1
+c = str[b:]
+d = float(c)
+print(d)
