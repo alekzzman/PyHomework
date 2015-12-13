30e10c49a4173018161bd6f80bfe10efb6cd1ef5
Alieksieienko Dmytro <alekzzman@gmail.com>
2015-12-13 22:04:50 +0200

    Doctest in exc4.2 added

--- exc4.2.py
+++ exc4.2.py
@@ -1,3 +1,23 @@
+"""
+Task:
+Write a program to read through
+a mail log, build a histogram using
+a dictionary to count how many messages
+have come from each email address, and print
+the dictionary.
+>>> print(adr, adress[adr])
+david.horwitz@uct.ac.za 4
+louis@media.berkeley.edu 3
+wagnermr@iupui.edu 1
+zqian@umich.edu 4
+gopal.ramasammycook@gmail.com 1
+antranig@caret.cam.ac.uk 1
+stephen.marquard@uct.ac.za 2
+cwen@iupui.edu 5
+gsilver@umich.edu 3
+ray@media.berkeley.edu 1
+rjlowe@iupui.edu 2
+"""
 words = []
 adress = {}
 txt = open('mbox-short.txt')
